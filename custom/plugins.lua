dc5cdef71df73f3a51104f32b9243c30804c24f4
Daniel Hirt <danielchirt16@gmail.com>
2023-06-01 19:17:51 -0400

    My NVIM configuration

---
+++ custom/plugins.lua
@@ -0,0 +1,10 @@
+local plugins = {
+  {
+  "nvim/nvim-lspconfig",
+  config = function()
+    require "plugins.configs.lspconfig"
+    require "custom.configs.lspconfig"
+  end,
+  }
+}
+return plugins
